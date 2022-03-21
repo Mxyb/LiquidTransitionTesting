07f68d09c33d72a214c265926a5105eee6adcc64
Mxyb <97235899+Mxyb@users.noreply.github.com>
2022-03-21 17:49:28 +0000

    Initial Commit

---
+++ LiquidTransitionTesting/LiquidTransitionTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidTransitionTestingApp.swift
+//  LiquidTransitionTesting
+//
+//  Created by Max Baker on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LiquidTransitionTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
